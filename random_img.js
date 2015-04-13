cf042b3d9373fe5685a45bd89ef3477b7e49ac4d
Alexander <agananiadis@gmail.com>
2015-04-12 21:11:58 -0400

    fun

--- random_img.js
+++ random_img.js
@@ -3,20 +3,20 @@ if (r < 0.14) {
   document.writeln('<img src="img/1.jpg" width:600 alt="Keep MatSci Weird" />');
 }
 else if (r >= 0.14 && r < 0.28) {
-  document.writeln('<img src="img/2.jpg" width:500 height:500 alt="Keep MatSci Weird" />');
+  document.writeln('<img src="img/2.jpg" alt="Keep MatSci Weird" />');
 }
 else if (r >= 0.28 && r < 0.42) {
-  document.writeln('<img src="img/3.jpg" width:500 height:500 alt="Keep MatSci Weird" />');
+  document.writeln('<img src="img/3.jpg" alt="Keep MatSci Weird" />');
 }
 else if (r >= 0.42 && r < 0.56) {
-  document.writeln('<img src="img/4.jpg" width:500 height:500 alt="Keep MatSci Weird" />');
+  document.writeln('<img src="img/4.jpg" alt="Keep MatSci Weird" />');
 }
 else if (r >= 0.56 && r < 0.70) {
-  document.writeln('<img src="img/5.jpg" width:500 height:500 alt="Keep MatSci Weird" />');
+  document.writeln('<img src="img/5.jpg" alt="Keep MatSci Weird" />');
 }
 else if (r >= 0.70 && r < 0.84) {
-  document.writeln('<img src="img/6.jpg" width:500 height:500 alt="Keep MatSci Weird" />');
+  document.writeln('<img src="img/6.jpg" alt="Keep MatSci Weird" />');
 }
 else {
-  document.writeln('<img src="img/7.jpg" width:500 height:500 alt="Keep MatSci Weird" />');
+  document.writeln('<img src="img/7.jpg" alt="Keep MatSci  Weird" />');
 }
