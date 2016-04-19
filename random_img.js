0d2441cdb76a7515f62e850bf4c64e8345032a51
Alexander Ananiadis <agananiadis@gmail.com>
2016-04-19 19:32:24 -0400

    Nate pics

--- random_img.js
+++ random_img.js
@@ -2,26 +2,32 @@
 if (r < 0.01) {
   document.writeln('<img src="img/8.jpg" alt="Keep MatSci Weird" />');
 }
-else if (r < 0.2) {
+else if (r < 0.1) {
   document.writeln('<img src="img/2.jpg" alt="Keep MatSci Weird" />');
 }
-else if (r < 0.4) {
+else if (r < 0.2) {
   document.writeln('<img src="img/3.jpg" alt="Keep MatSci Weird" />');
 }
-else if (r < 0.5) {
+else if (r < 0.3) {
   document.writeln('<img src="img/4.jpg" alt="Keep MatSci Weird" />');
 }
-else if (r < 0.6) {
+else if (r < 0.4) {
   document.writeln('<img src="img/5.jpg" alt="Keep MatSci Weird" />');
 }
-else if (r < 0.7) {
+else if (r < 0.5) {
   document.writeln('<img src="img/6.jpg" alt="Keep MatSci Weird" />');
 }
-else if (r < 0.8) {
+else if (r < 0.6) {
   document.writeln('<img src="img/7.jpg" alt="Keep MatSci Weird" />');
 }
-else if (r < 0.97) {
+else if (r < 0.7) {
   document.writeln('<img src="img/1.jpg" alt="Keep MatSci Weird" />');
+}
+else if (r < 0.8) {
+  document.writeln('<img src="img/11.jpg" alt="Keep MatSci Weird" />');
+}
+else if (r < 0.97) {
+  document.writeln('<img src="img/12.jpg" alt="Keep MatSci Weird" />');
 }
 else {
   document.writeln('<img src="img/9.jpg" alt="Keep MatSci  Weird" />');
